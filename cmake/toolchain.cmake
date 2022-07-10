cf92ad852db7c61408bddbdd599a5fdcf3698997
Rob Gowin <robgowin@gmail.com>
2022-07-10 08:39:12 -0500

    Add cmake support files.

---
+++ cmake/toolchain.cmake
@@ -0,0 +1,11 @@
+# Not defined: CMAKE_ASM_COMPILER CMAKE_CXX_COMPILER
+set(CMAKE_AR           m68k-atari-mint-gcc-ar)
+set(CMAKE_C_COMPILER   m68k-atari-mint-gcc-4.6.4)
+set(CMAKE_C_LINKER     m68k-atari-mint-ld)
+set(CMAKE_C_OBJCOPY    m68k-atari-mint-objcopy)
+set(CMAKE_C_RANLIB     m68k-atari-mint-gcc-ranlib)
+set(CMAKE_C_SIZE       m68k-atari-mint-size)
+set(CMAKE_C_STRIP      m68k-atari-mint-strip)
+
+set(CMAKE_SYSTEM_NAME "TOS")
+set(CMAKE_SYSTEM_PROCESSOR m68000)
